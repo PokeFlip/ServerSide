2b8f68e9dcebaac0acccce8afccf94c32c4f8d0d
G.Provost <grace.g.provost@gmail.com>
2017-12-16 13:34:36 -0800

    modified env variable. changed function names

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// For Richard .env: DATABASE_URL=postgres://postgres:password@localhost:5432/pokeflip
+
 require('dotenv').config();
 const express = require('express');
 const app = express();
@@ -18,8 +20,8 @@ app.use(cors());
 app.use(body.json());
 app.use(body.urlencoded({extended: true}));
 
-loadLeaderboard();
-loadPokemon();
+loadLeaderboardTable();
+loadPokemonTable();
 
 app.get('/game/:id', (req, res) => {
     const id = req.params.id;
@@ -28,7 +30,7 @@ app.get('/game/:id', (req, res) => {
         .then((resp) => {
             const poke = resp.body;
             const typeSlot = () => {
-                return (poke.types.length > 1) ? poke.types[1].type.name : poke.types[0].type.name;
+                return (poke.types.length > 1) ? poke.types[1].type.name : poke.types[0].type.name; //if the pokemon has two types, select the main type
             };
             const pokeObj = {
                 name: poke.name || 'n/a',
@@ -38,28 +40,27 @@ app.get('/game/:id', (req, res) => {
             };
             insertPokemon(pokeObj);
             res.send('done');
-        });
-
-
+        })
+        .catch(console.error);
 });
 
 app.listen(PORT, () => (console.log(`listening for api requests to ${PORT}`)));
 
-app.get('/pokemon/:type', (req, res) => { // PROBABLY NOT WORKING
-    const type = req.params.type;
-    superagent
-        .get(`${dexUrl}${type}/`)
-        .then((res) => {
-            const pokeReturn = res.body.items.slice(0,5).map( poke => {
-                return {
-                    name: poke.name || 'n/a',
-                    dex_number: poke.id || 'n/a',
-                    img_url: poke.sprites.front_default || 'n/a',
-                    description: poke.flavor_text_entries || 'n/a'
-                };
-            });
-        });
-});
+// app.get('/pokemon/:type', (req, res) => { // PROBABLY NOT WORKING
+//     const type = req.params.type;
+//     superagent
+//         .get(`${dexUrl}${type}/`)
+//         .then((res) => {
+//             const pokeReturn = res.body.items.slice(0,5).map( poke => {
+//                 return {
+//                     name: poke.name || 'n/a',
+//                     dex_number: poke.id || 'n/a',
+//                     img_url: poke.sprites.front_default || 'n/a',
+//                     description: poke.flavor_text_entries || 'n/a'
+//                 };
+//             });
+//         });
+// });
 
 //////// ** DATABASE LOADERS ** ////////
 ////////////////////////////////////////
@@ -72,15 +73,16 @@ function insertPokemon(poke) {
         .catch(console.error);
 }
 
-function loadLeaderboard() {
-    client.query(`CREATE TABLE IF NOT EXISTS leaderboard (id serial PRIMARY KEY, name VARCHAR(50), score INTEGER);`
+function loadLeaderboardTable() {
+    client.query(
+        `CREATE TABLE IF NOT EXISTS leaderboard (id serial PRIMARY KEY, name VARCHAR(50), score INTEGER);`
     )
         .catch(console.error);
 }
 
-function loadPokemon() {
-    client.query(`CREATE TABLE IF NOT EXISTS pokemon (id serial PRIMARY KEY, dex_number INTEGER UNIQUE, name VARCHAR(25), img_url VARCHAR(300), type VARCHAR(25));`
+function loadPokemonTable() {
+    client.query(
+        `CREATE TABLE IF NOT EXISTS pokemon (id serial PRIMARY KEY, dex_number INTEGER UNIQUE, name VARCHAR(25), img_url VARCHAR(300), type VARCHAR(25));`
     )
-        .then(loadPokemon)
         .catch(console.error);
 }
