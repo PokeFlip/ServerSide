52e4fe162c6f65fe628c191ebe650167e0f34096
Richard Livingston <livingston.richard@gmail.com>
2017-12-18 11:20:33 -0800

    We added a route for querying the leaderboard names and scores, and we created a route for posting leaderboard names and scores. Also created a route that will deliver a 404 error status when the requested route does not exist

--- server.js
+++ server.js
@@ -62,9 +62,24 @@ app.get('/pokemonspecies/:dex', (req, res) => {
             res.send(findEnglishDexEntry(allEntries)); //looks for english entry
         });
 });
-//TODO: create route for posting leaderboard names and scores.
+//TODO DONE: create route for querying leaderbord names and scores.
+app.get('/leaderboard', (req, res) => { // not tested
+    client.query('SELECT * FROM leaderboard LIMIT 10')
+        .then(data => res.send(data.rows))
+        .catch(console.error);
+});
+//TODO DONE: create route for posting leaderboard names and scores.
+app.post('/leaderboard/:name/:score', (req, res) => { // not tested
+    console.log(req.body);
+    client.query('INSERT INTO leaderboard (name, score) VALUES ($1, $2)', [req.params.name, req.params.score])
+        .then(() => res.send('done'))
+        .catch(console.error);
+});
+//TODO DONE: Will deliver 404 status message to the user when the requested route does not exist.
+app.get('*', (request, response) => { // not tested
+    response.status('404').send({error:'Not found!'});
+});
 
-//TODO: create route for querying leaderbord names and scores.
 
 app.listen(PORT, () => (console.log(`listening for api requests to ${PORT}`)));
 
